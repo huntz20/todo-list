ad5f33740ed0aa82d95d457b7f5e22ddb2d8d1bb
huntz20 <huntzrahmadi@gmail.com>
2022-12-23 21:17:28 +0700

    create Modal component and few style adjustment

---
+++ src/component/Modal.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import crossIcon from "../assets/cross.svg"
+import classNames from "classnames";
+const Modal = ({show = false}) => {
+    return <>
+        <div className={classNames("fixed top-0 left-0 right-0 z-50 w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full bg-shadow", {hidden: !show})}>
+            <div className="absolute translate-y-[-50%] translate-x-[-50%] top-[50%] left-[50%] w-[30%] h-[auto]">
+                <div className="relative bg-white rounded-lg shadow">
+                    <div className="flex items-start justify-between p-4 rounded-t">
+                        <h3 className="text-xl font-semibold text-black-400">
+                            Terms of Service
+                        </h3>
+                        <button type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="defaultModal">
+                            <img src={crossIcon} alt="cross-icon" width="24"/>
+                            <span className="sr-only">Close modal</span>
+                        </button>
+                    </div>
+                    <div className="p-6 space-y-6">
+                        <p className="text-base leading-relaxed text-gray-500">
+                            With less than a month to go before the European Union enacts new consumer privacy laws for its citizens, companies around the world are updating their terms of service agreements to comply.
+                        </p>
+                        <p className="text-base leading-relaxed text-gray-500">
+                            The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25 and is meant to ensure a common set of data rights in the European Union. It requires organizations to notify users as soon as possible of high-risk data breaches that could personally affect them.
+                        </p>
+                    </div>
+                    <div className="flex items-center p-6 space-x-2 rounded-b">
+                        <button data-modal-toggle="defaultModal" type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">I accept</button>
+                        <button data-modal-toggle="defaultModal" type="button" className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Decline</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </>
+}
+
+
+export default Modal
--- src/component/TodoCard.jsx
+++ src/component/TodoCard.jsx
@@ -1,31 +1,31 @@
 import React from 'react'
 import addIcon from '../assets/add-svgrepo-com.svg'
 import TaskCard from "./TaskCard.jsx";
-const TodoCard = ({}) => {
+const TodoCard = ({isLoading}) => {
     const renderNoTask = () => {
         return <div className="bg-[#f3f3f3] border-2 border-[#e3e3e3] rounded-sm py-1 px-4"><span className="font-light">No Task</span></div>
     }
 
     const renderLoadingTask = () => {
         return <div className="basis-1/4 border-2 rounded py-6 px-4 animate-pulse mb-4">
-            <div className="h-4 bg-gray-400 rounded-md w-[200px] mb-4"></div>
+            <div className="h-4 bg-gray-400 rounded-md w-[30%] mb-4"></div>
             <hr className="dashed-line"/>
             <div className="flex items-center">
-                <div className="h-[20px] bg-gray-400 rounded-full w-[230px] mr-2"></div>
+                <div className="h-[20px] bg-gray-400 rounded-full w-[70%] mr-4"></div>
                 <div className="h-[20px] bg-gray-400 rounded-full w-[20px]"></div>
             </div>
         </div>
     }
 
     const renderLoading = () => {
-        return <div className="basis-1/4 border-2 rounded py-4 px-2 animate-pulse">
-            <div className="h-4 bg-gray-400 rounded-md w-[80px] mb-4"></div>
-            <div className="h-3 bg-gray-400 rounded-md w-[120px] mb-4"></div>
+        return <div className="border-2 rounded p-4 animate-pulse">
+            <div className="h-4 bg-gray-400 rounded-md w-[20%] mb-4"></div>
+            <div className="h-3 bg-gray-400 rounded-md w-[30%] mb-4"></div>
             <div>
                 {renderLoadingTask()}
                 {renderLoadingTask()}
             </div>
-            <div className="flex items-center">
+            <div className="flex items-center cursor-not-allowed">
                 <img className="inline-block mr-2 " width="18" src={addIcon} alt="add-icon"/>
                 <span className="font-light text-sm">New Task</span>
             </div>
@@ -37,7 +37,8 @@ const TodoCard = ({}) => {
             <TaskCard />
         </div>
     }
-    // return renderLoading()
+    if (isLoading) return renderLoading();
+
     return <div className="border-2 rounded p-4">
         <span className="border-2 font-light p-[6px] rounded-md inline-block mb-1">
             Group Name
